30d944b51a129922971d7c93dd29c8072d8b11ee
Arturo Torres Sánchez <r2d2.art2005@gmail.com>
2018-05-24 21:26:09 -0500

    Update test.js to use `flat` instead

--- test.js
+++ test.js
@@ -1,25 +1,8 @@
 const test = require('tape');
 require('./index');
 
-test('smoosh picks the first item', t => {
+test('flat picks the first item', t => {
   t.plan(1);
   const arr = [10, 20, 30, 40];
-  t.equals(arr.smoosh(), 10);
-});
-
-test('smooshMap picks the first item then maps it', t => {
-  t.plan(1);
-  const arr = [5, 6, 7, 8];
-  t.equals(arr.smooshMap(x => x * 0.5), 2.5);
-});
-
-test('smooshMap does not call fn on an empty array', t => {
-  t.plan(1);
-  const arr = [];
-  t.equals(
-    arr.smooshMap(x => {
-      throw new Error('Unexpected');
-    }),
-    undefined,
-  );
+  t.equals(arr.flat(), 10);
 });
